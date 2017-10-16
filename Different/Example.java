188bc7a57019ff11722e809c5f732c7756cf4e53
KirillSergeevich <kirill.ivonchik@gmail.com>
2017-10-16 19:56:22 +0300

    Some additional files were added

---
+++ Different/Example.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TTest  {
+
+    static int a = 5;
+    float f = 2.0f;
+    private static final int MY_CONST = 5;
+    static String myStr = "hello";
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = "first";
+        String a = "AA";
+        String b = "aa";
+        if (a.equalsIgnoreCase(b))
+        while (!word.equals("0")) {
+            word = sc.nextLine();
+            translate(word);
+        }
+        for (int i = 5; i < 120; i++) {
+
+        }
+    }
+
+    static private String translate(String word) {
+        return null;
+    }
+
+    static private void method(int a) {
+        System.out.println(a);
+    }
+}
